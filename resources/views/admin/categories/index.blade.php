717b8a2850d605918f906efbba272f70bf498475
gragemediatechnology <gragemediatechnology@gmail.com>
2024-10-07 15:16:51 +0700

    update

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -54,7 +54,7 @@
                                         <div class="flex items-center text-sm">
                                             <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
                                                 <img class="object-cover w-full h-full rounded-full"
-                                                    src="{{ https://lapakkbk.online/($category->icon) }}" alt="{{ $category->name }}"
+                                                    src="https://lapakkbk.online/{{ $category->icon}}" alt="{{ $category->name }}"
                                                     loading="lazy" />
                                                 <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true">
                                                 </div>
