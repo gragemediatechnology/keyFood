d52e41c63bea867236be2868a8a286e33a0f56cd
jipyy <jahdanpandita16@gmail.com>
2024-10-08 09:28:43 +0700

    update

--- resources/views/partials/profile.blade.php
+++ resources/views/partials/profile.blade.php
@@ -183,7 +183,7 @@
             </a>
             <button type="button" data-modal-target="popup-modal" data-modal-toggle="popup-modal"
                 class="text-white mt-3 bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-                <i class='bx bx-trash'></i> Delete Account
+                <i class='bx bx-trash'></i> Hapus Akun
             </button>
             <div id="popup-modal" tabindex="-1"
                 class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
@@ -205,19 +205,18 @@
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                     stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                             </svg>
-                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want
-                                to delete your account?</h3>
-                            <form action="{{ route('profile.destroy') }}" method="POST" class="mt-6">
+                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Apakah anda yakin ingin menghapus permanen akun anda?</h3>
+                            <form action="/edit-profile/destroy" method="POST" class="mt-6">
                                 @csrf
                                 @method('DELETE')
                                 <button data-modal-hide="popup-modal" type="submit"
                                     class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                                    Yes, I'm sure
+                                    Ya, saya setuju
                                 </button>
                             </form>
                             <button data-modal-hide="popup-modal" type="button"
                                 class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No,
-                                cancel</button>
+                                batal</button>
                         </div>
                     </div>
                 </div>
