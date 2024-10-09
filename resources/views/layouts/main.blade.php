4987b99a619880ca374b147954127d6851274947
Baihaqi34 <abay999mmm@gmail.com>
2024-10-09 13:03:25 +0700

    sasas

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -73,7 +73,7 @@
     <link rel="stylesheet"
         href="https://rawcdn.githack.com/jipyy/keyFood/94e3005f001914148945e309f555715db94e24f6/public/css/nav.css"> -->
 
-    @if(Route::currentRouteName() == '/')
+    @if(Route::currentRouteName() == 'home')
         <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/clock.css">
         <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/profile.css">
         <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/home.css">
