2d2189a4f4364aa8bf84ffd2f91081e0750058ca
Baihaqi34 <abay999mmm@gmail.com>
2024-10-09 08:59:12 +0700

    dada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,8 +30,9 @@
                     <div class="clock__date">
                         <!-- <span id="date-day-week"></span> -->
                         <span id="date-day"></span>
-                        <span id="date-month">ok</span>
+                        <span id="date-month"></span>
                         <span id="date-year"></span>
+                        <p>haha</p>
                     </div>
                     <div class="text-container">
                         <p>*Catatan: Direkomendasikan Dibuka Melalui Ponsel</p>
