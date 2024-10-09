fc5e6497a92abdd5579798b6fc281c3c496d31f6
Baihaqi34 <abay999mmm@gmail.com>
2024-10-09 14:36:53 +0700

    fsfsf

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -24,23 +24,23 @@
 
 
 
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/clock.css">
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/profile.css">
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/home.css">
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/style.css">
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/load.css">
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/app.css">
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/halaman-toko.css">
-    <link rel="preconnect"
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/clock.css">
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/profile.css">
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/home.css">
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/style.css">
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/load.css">
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/app.css">
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/halaman-toko.css">
+    <link rel="stylesheet"
         href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/product-slider.css">
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/stores.css">
-    <link rel="preconnect"
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/stores.css">
+    <link rel="stylesheet"
         href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/home-container.css">
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/categories.css">
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/checkout.css">
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/cart.css">
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/history.css">
-    <link rel="preconnect" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/nav.css">
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/categories.css">
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/checkout.css">
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/cart.css">
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/history.css">
+    <link rel="stylesheet" href="https://raw.githack.com/gragemediatechnology/keyFood/main/public/css/nav.css">
 
     <!-- <link rel="stylesheet"
         href="https://rawcdn.githack.com/jipyy/keyFood/94e3005f001914148945e309f555715db94e24f6/public/css/profile.css">
