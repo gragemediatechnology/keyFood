80c3393ec65b81f4680e5748f0f69911c59a84b6
jipyy <jahdanpandita16@gmail.com>
2024-10-09 11:11:14 +0700

    update

--- resources/views/stores.blade.php
+++ resources/views/stores.blade.php
@@ -3,7 +3,7 @@
 <section id="home">
 
     <div class="contain">
-        <form class="search-bar">
+        {{-- <form class="search-bar">
             <label for="default-search"
                 class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div class="relative">
@@ -20,7 +20,7 @@
                 <button type="submit"
                     class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
-        </form>
+        </form> --}}
     </div>
 
     <!-- content asli -->
