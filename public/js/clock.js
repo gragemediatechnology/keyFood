6d066199f37a4e5e0b9affba313a53fb1676c5a4
Baihaqi34 <abay999mmm@gmail.com>
2024-10-09 09:03:25 +0700

    coba

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,65 +1,65 @@
-// Function to handle clock display
-const initializeClock = () => {
-    const hour = document.getElementById('clock-hour'),
-        minutes = document.getElementById('clock-minutes'),
-        seconds = document.getElementById('clock-seconds');
+// // Function to handle clock display
+// const initializeClock = () => {
+//     const hour = document.getElementById('clock-hour'),
+//         minutes = document.getElementById('clock-minutes'),
+//         seconds = document.getElementById('clock-seconds');
 
-    const clock = () => {
-        let date = new Date();
-        let hh = date.getHours() * 30,
-            mm = date.getMinutes() * 6,
-            ss = date.getSeconds() * 6;
+//     const clock = () => {
+//         let date = new Date();
+//         let hh = date.getHours() * 30,
+//             mm = date.getMinutes() * 6,
+//             ss = date.getSeconds() * 6;
 
-        hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
-        minutes.style.transform = `rotateZ(${mm}deg)`;
-        seconds.style.transform = `rotateZ(${ss}deg)`;
-    };
-    setInterval(clock, 1000); // 1000 = 1s
+//         hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+//         minutes.style.transform = `rotateZ(${mm}deg)`;
+//         seconds.style.transform = `rotateZ(${ss}deg)`;
+//     };
+//     setInterval(clock, 1000); // 1000 = 1s
 
-    const textHour = document.getElementById('text-hour'),
-        textMinutes = document.getElementById('text-minutes'),
-        textAmPm = document.getElementById('text-ampm'),
-        dateDay = document.getElementById('date-day'),
-        dateMonth = document.getElementById('date-month'),
-        dateYear = document.getElementById('date-year');
+//     const textHour = document.getElementById('text-hour'),
+//         textMinutes = document.getElementById('text-minutes'),
+//         textAmPm = document.getElementById('text-ampm'),
+//         dateDay = document.getElementById('date-day'),
+//         dateMonth = document.getElementById('date-month'),
+//         dateYear = document.getElementById('date-year');
 
-    const clockText = () => {
-        let date = new Date();
-        let hh = date.getHours(),
-            ampm,
-            mm = date.getMinutes(),
-            day = date.getDate(),
-            month = date.getMonth(),
-            year = date.getFullYear();
+//     const clockText = () => {
+//         let date = new Date();
+//         let hh = date.getHours(),
+//             ampm,
+//             mm = date.getMinutes(),
+//             day = date.getDate(),
+//             month = date.getMonth(),
+//             year = date.getFullYear();
 
-        if (hh >= 12) {
-            hh = hh - 12;
-            ampm = 'PM';
-        } else {
-            ampm = 'AM';
-        }
+//         if (hh >= 12) {
+//             hh = hh - 12;
+//             ampm = 'PM';
+//         } else {
+//             ampm = 'AM';
+//         }
 
-        if (hh == 0) { hh = 12; }
+//         if (hh == 0) { hh = 12; }
 
-        if (hh < 10) { hh = `0${hh}`; }
+//         if (hh < 10) { hh = `0${hh}`; }
 
-        textHour.innerHTML = `${hh}:`;
+//         textHour.innerHTML = `${hh}:`;
 
-        if (mm < 10) { mm = `0${mm}`; }
+//         if (mm < 10) { mm = `0${mm}`; }
 
-        textMinutes.innerHTML = mm;
-        textAmPm.innerHTML = ampm;
+//         textMinutes.innerHTML = mm;
+//         textAmPm.innerHTML = ampm;
 
-        // Nama bulan dalam bahasa Indonesia
-        let months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+//         // Nama bulan dalam bahasa Indonesia
+//         let months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
 
-        dateDay.innerHTML = day;
-        // dateMonth.innerHTML = `${months[month]},`;
-        dateMonth.innerHTML = `Oktober,`;
-        dateYear.innerHTML = year;
-    };
-    setInterval(clockText, 1000); // 1000 = 1s
-};
+//         dateDay.innerHTML = day;
+//         // dateMonth.innerHTML = `${months[month]},`;
+//         dateMonth.innerHTML = `${months[month]},`;
+//         dateYear.innerHTML = year;
+//     };
+//     setInterval(clockText, 1000); // 1000 = 1s
+// };
 
 // Function to handle theme toggle
 const initializeTheme = () => {
