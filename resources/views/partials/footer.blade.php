65a207ac355d91edc06d63b9c3ac81c2bdf9f78a
jipyy <jahdanpandita16@gmail.com>
2024-10-10 11:54:09 +0700

    update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,13 @@
-
 <footer class="footer mt-5" id="footer">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
-            @foreach($cms as $company)
-                <a href="/home" class="flex items-center">
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap text-gray-500">{{$company->company_name}}</span>
-                    <img src="https://lapakkbk.online/{{ $company->logo }}" class="h-8 me-2" alt="KeyFood Logo" />
-                </a>
+                @foreach ($cms as $company)
+                    <a href="/home" class="flex items-center">
+                        <span
+                            class="self-center text-2xl font-semibold whitespace-nowrap text-gray-500">{{ $company->company_name }}</span>
+                        <img src="https://lapakkbk.online/{{ $company->logo }}" class="h-8 me-2" alt="KeyFood Logo" />
+                    </a>
             </div>
             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
                 <div>
@@ -34,14 +34,17 @@
                     <h2 class="mb-6 text-sm font-semibold text-gray-400 uppercase">Lapak KBK</h2>
                     <ul class="text-gray-500 font-medium">
                         <li class="mb-4">
-                            <a href="https://maps.app.goo.gl/jPYA3x3XgB3pXWSYA" class="hover:underline ">{{$company->company_name}}
-                            {{$company->lokasi}}</a>
+                            <a href="https://maps.app.goo.gl/jPYA3x3XgB3pXWSYA"
+                                class="hover:underline ">{{ $company->company_name }}
+                                {{ $company->lokasi }}</a>
                         </li>
                         <li class="mb-4">
-                            <a href="mailto:andikasupriyadi27@gmail.com" class="hover:underline">{{$company->email}}</a>
+                            <a href="mailto:andikasupriyadi27@gmail.com"
+                                class="hover:underline">{{ $company->email }}</a>
                         </li>
                         <li>
-                            <a href="https://wa.me/{{$company->phone}}" class="hover:underline">+62{{$company->phone}}</a>
+                            <a href="https://wa.me/{{ $company->phone }}"
+                                class="hover:underline">+62{{ $company->phone }}</a>
                         </li>
                     </ul>
                 </div>
@@ -50,10 +53,10 @@
         <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div class="sm:flex sm:items-center sm:justify-between">
             <span class="text-sm text-gray-500 sm:text-center">© 2024 <a href="#"
-                    class="hover:underline">{{$company->company_name}}™</a>. All Rights Reserved.
+                    class="hover:underline">{{ $company->company_name }}™</a>. All Rights Reserved.
             </span>
             <div class="flex mt-4 sm:justify-center sm:mt-0">
-                <a href="#" class="text-gray-500 hover:text-gray-500">
+                {{-- <a href="#" class="text-gray-500 hover:text-gray-500">
                     <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                         viewBox="0 0 8 19">
                         <path fill-rule="evenodd"
@@ -61,42 +64,42 @@
                             clip-rule="evenodd" />
                     </svg>
                     <span class="sr-only">Facebook page</span>
-                </a>
-                <a href="#" class="text-gray-500 hover:text-gray-500 ms-5">
+                </a> --}}
+
+
+                <!-- YouTube Icon -->
+                <a href="https://youtube.com/@gragemediatechnology?si=TUzeCE_g9uFOHmda" class="text-gray-500 hover:text-gray-500">
                     <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                        viewBox="0 0 21 16">
-                        <path
-                            d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
-                    </svg>
-                    <span class="sr-only">Discord community</span>
-                </a>
-                <a href="#" class="text-gray-500 hover:text-gray-500 ms-5">
-                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                        viewBox="0 0 20 17">
+                        viewBox="0 0 24 24">
                         <path fill-rule="evenodd"
-                            d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
+                            d="M23.5 6.25a3.002 3.002 0 0 0-2.106-2.122C19.17 3.5 12 3.5 12 3.5s-7.171 0-9.395.63A3.002 3.002 0 0 0 .5 6.25 29.208 29.208 0 0 0 0 12c.005 1.937.111 3.875.5 5.75a3.002 3.002 0 0 0 2.106 2.122C4.83 20.5 12 20.5 12 20.5s7.171 0 9.395-.63A3.002 3.002 0 0 0 23.5 17.75c.39-1.875.495-3.813.5-5.75a29.208 29.208 0 0 0-.5-5.75ZM9.75 15.25v-6l6 3-6 3Z"
                             clip-rule="evenodd" />
                     </svg>
-                    <span class="sr-only">Twitter page</span>
+                    <span class="sr-only">YouTube channel</span>
                 </a>
-                <a href="#" class="text-gray-500 hover:text-gray-500 ms-5">
+
+                <!-- Instagram Icon -->
+                <a href="https://www.instagram.com/pandaidigital_idn?igsh=MXM4aDBxcnprbDR1eQ==" class="text-gray-500 hover:text-gray-500">
                     <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                        viewBox="0 0 20 20">
+                        viewBox="0 0 24 24">
                         <path fill-rule="evenodd"
-                            d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                            d="M12 0a7.838 7.838 0 0 0-7.853 7.853v8.294A7.838 7.838 0 0 0 12 24a7.838 7.838 0 0 0 7.853-7.853V7.853A7.838 7.838 0 0 0 12 0Zm0 1.5a6.338 6.338 0 0 1 6.353 6.353v8.294A6.338 6.338 0 0 1 12 22.5a6.338 6.338 0 0 1-6.353-6.353V7.853A6.338 6.338 0 0 1 12 1.5ZM12 5.25a6.75 6.75 0 1 1 0 13.5 6.75 6.75 0 0 1 0-13.5Zm5.053-1.125a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z"
                             clip-rule="evenodd" />
                     </svg>
-                    <span class="sr-only">GitHub account</span>
+                    <span class="sr-only">Instagram page</span>
                 </a>
-                <a href="#" class="text-gray-500 hover:text-gray-500 ms-5">
+
+                <!-- Website Icon -->
+                <a href="https://grageweb.online" class="text-gray-500 hover:text-gray-500">
                     <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                        viewBox="0 0 20 20">
+                        viewBox="0 0 24 24">
                         <path fill-rule="evenodd"
-                            d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
+                            d="M12 2.25a9.75 9.75 0 1 0 0 19.5 9.75 9.75 0 0 0 0-19.5ZM12 3.75c.664 0 1.331.087 1.985.248a12.179 12.179 0 0 0-1.853 3.933c-.044.13-.084.257-.123.384h-2.018a12.179 12.179 0 0 0-1.853-3.933c.654-.161 1.32-.248 1.985-.248Zm3.682 1.07A8.252 8.252 0 0 1 18.403 9h-2.547a11.726 11.726 0 0 0-1.726-4.18c.521.268 1.018.603 1.552 1.001Zm-7.364 0A11.726 11.726 0 0 0 6.865 9H4.318a8.252 8.252 0 0 1 2.721-4.18c.534-.398 1.03-.733 1.552-1.001ZM12 8.25c.313 0 .626.049.935.147a9.711 9.711 0 0 0-.935 3.228 9.711 9.711 0 0 0-.935-3.228c.309-.098.622-.147.935-.147ZM9.41 9c.123.257.24.525.347.803.38 1.033.616 2.25.66 3.447H7.043c.05-1.197.279-2.414.66-3.447.106-.278.223-.546.347-.803ZM12 12.75c.315 0 .627-.022.935-.065-.322 2.035-1.122 3.766-1.87 4.79-.392-.469-.887-1.18-1.341-2.166-.509-1.091-.855-2.283-.955-3.394h2.231Zm4.943 0c-.05 1.197-.279 2.414-.66 3.447a9.717 9.717 0 0 1-.347.803c-.123-.257-.24-.525-.347-.803-.38-1.033-.616-2.25-.66-3.447h2.014ZM7.043 15h2.361c.097 1.111.446 2.303.955 3.394.454.986.949 1.697 1.341 2.166a10.1 10.1 0 0 1-1.87-4.79c-.308.043-.62.065-.935.065-.313 0-.626-.022-.935-.065Zm7.496 0h2.361a8.252 8.252 0 0 1-2.721 4.18 11.726 11.726 0 0 0 1.726-4.18Zm-4.122 4.931a12.179 12.179 0 0 1-1.853-3.933h2.018c.039.127.079.254.123.384.389 1.06.905 2.073 1.853 3.933a8.258 8.258 0 0 1-2.142-.384ZM18.403 9a8.252 8.252 0 0 1-2.721 4.18 11.726 11.726 0 0 0 1.726-4.18h2.547Zm-10.038 0A11.726 11.726 0 0 0 6.865 9h-2.547a8.252 8.252 0 0 1 2.721-4.18Z"
                             clip-rule="evenodd" />
                     </svg>
-                    <span class="sr-only">Dribbble account</span>
+                    <span class="sr-only">Website</span>
                 </a>
+
             </div>
         </div>
     </div>
