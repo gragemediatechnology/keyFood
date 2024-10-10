ff0166c9515f8739436390f94e266f81c7beb33c
Baihaqi34 <abay999mmm@gmail.com>
2024-10-10 08:46:29 +0700

    dggggggd

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -78,8 +78,8 @@
                             @foreach ($stores as $store)
                                 <form action="/detailed-store" method="POST">
                                     @csrf
-                                    <input type="hidden" value="{{ $store->nama_toko }}" name="nama_toko">
                                     <button type="submit">
+                                        <input type="hidden" value="{{ $store->nama_toko }}" name="nama_toko">
                                     <tr class="text-gray-700 dark:text-gray-400">
                                         {{-- ID Toko --}}
                                         <td class="px-4 py-3">
