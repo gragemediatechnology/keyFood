7c3578ccbaeb374ccf7009515e85b4ba2d4d5395
jipyy <jahdanpandita16@gmail.com>
2024-10-11 11:34:16 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -173,8 +173,8 @@
                                             </form>
                                         @else
                                             <!-- Jika user belum login, arahkan ke halaman login dengan redirect -->
-                                            {{-- <a href="{{ config('app.url') . '/log-reg?redirect=' . urlencode(request()->fullUrl()) }}" class="btn">Lihat Produk</a> --}}
-                                            <a href="{{ config(('app.url') .'/login?redirect=' . urlencode(request()->fullUrl())) }}" class="btn">Lihat Produk</a>
+                                            <a href="{{ config('app.url') . '/log-reg?redirect=' . urlencode(request()->fullUrl()) }}" class="btn">Lihat Produk</a>
+                                            {{-- <a href="{{ config(('app.url') .'/login?redirect=' . urlencode(request()->fullUrl())) }}" class="btn">Lihat Produk</a> --}}
 
                                         @endif
                                     </div>
