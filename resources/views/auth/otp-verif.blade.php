b350f20a34b12baa4c40d02b43aeadd4ec067884
jipyy <jahdanpandita16@gmail.com>
2024-10-16 13:46:49 +0700

    gitpuil

--- resources/views/auth/otp-verif.blade.php
+++ resources/views/auth/otp-verif.blade.php
@@ -166,7 +166,7 @@
                     });
                     if (response.ok) {
                         //sumber masalah
-                        window.location.href = "/";
+                        window.location.href = "{{ route('home') }}";
                         // document.getElementById('otp-form').submit();
                     } else {
                         const errorData = await response.json();
