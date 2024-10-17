11c4103c3e87b29f44fe58fa5bc6f4ccf48f05dd
Baihaqi34 <abay999mmm@gmail.com>
2024-10-17 09:00:43 +0700

    assa

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -76,7 +76,7 @@
                         </thead>
                         <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
                             @foreach ($stores as $store)
-                                <form action={{"/detailed-store" . $store->id_toko }} method="GET">
+                                <form action="/detailed-store" method="GET">
                                     <input type="hidden" value="{{ $store->id_toko }}" name="id">
 
 
