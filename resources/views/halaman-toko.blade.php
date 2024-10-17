5ef654617172bde374429870fb731780d6d8a435
Baihaqi34 <abay999mmm@gmail.com>
2024-10-17 09:21:41 +0700

    sasa

--- resources/views/halaman-toko.blade.php
+++ resources/views/halaman-toko.blade.php
@@ -60,11 +60,12 @@
                                 <span hidden>{{ $product->slug }}</span>
                                 <img alt="{{ $product->name }}" src="https://lapakkbk.online/{{ $product->photo }}">
                                 <strong>{{ $product->name }}</strong>
-                                <span class="quantity">Kategori: {{ $product->category ? $product->category->name : 'Unknown' }}</span>
+                                {{-- <strong>{{ $product->toko->nama_toko }}</strong> --}}
+                                <span class="quantity">Kategori:
+                                    {{ $product->category ? $product->category->name : 'Unknown' }}</span>
                                 <span class="quantity">Toko: {{ $product->toko ? $product->toko->nama_toko : 'Unknown' }}</span>
-
-                                {{-- Rating bintang --}}
-                                <div class="flex justify-center">
+                                <div class="flex">
+                                    {{-- Tampilkan bintang penuh --}}
                                     @for ($i = 1; $i <= $fullStars; $i++)
                                         <svg xmlns="http://www.w3.org/2000/svg" class="text-yellow-500 w-5 h-auto fill-current"
                                             viewBox="0 0 16 16">
@@ -72,6 +73,8 @@
                                                 d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
                                         </svg>
                                     @endfor
+
+                                    {{-- Tampilkan setengah bintang jika ada --}}
                                     @if ($halfStar)
                                         <svg xmlns="http://www.w3.org/2000/svg" class="text-yellow-500 w-5 h-auto fill-current"
                                             viewBox="0 0 16 16">
@@ -79,11 +82,28 @@
                                                 d="M8 12.545L3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 12.545V0z" />
                                         </svg>
                                     @endif
-                                </div>
 
-                                {{-- Tambahkan menu khusus admin --}}
-                                <a href="#" class="cart-btn">
-                                    <i class="fas fa-cogs"></i> Admin Menu
+                                    {{-- Tampilkan bintang kosong --}}
+                                    @for ($i = 1; $i <= $emptyStars; $i++)
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-300 w-5 h-auto fill-current"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                        </svg>
+                                    @endfor
+
+                                    @if ($average_rating >= 1)
+                                        <p class="mx-2">( {{ $average_rating }} / 5 )</p>
+                                    @else
+                                        <p class="mx-2">( 0 / 0 )</p>
+                                    @endif
+                                </div>
+                                <span class="quantity"></span>
+                                <span class="price">Rp {{ number_format($product->price, 0, ',', '.') }}</span>
+                                <a href="javascript:void(0)" data-product-id="{{ $product->id }}"
+                                    data-store-id="{{ $product->store_id }}" data-category-id="{{ $product->category_id }}"
+                                    data-slug="{{ $product->slug }}" class="cart-btn">
+                                    <i class="fas fa-shopping-bag"></i> Tambah Ke Keranjang
                                 </a>
                             </div>
 
@@ -96,11 +116,12 @@
                                 <span hidden>{{ $product->slug }}</span>
                                 <img alt="{{ $product->name }}" src="https://lapakkbk.online/{{ $product->photo }}">
                                 <strong>{{ $product->name }}</strong>
-                                <span class="quantity">Kategori: {{ $product->category ? $product->category->name : 'Unknown' }}</span>
+                                {{-- <strong>{{ $product->toko->nama_toko }}</strong> --}}
+                                <span class="quantity">Kategori:
+                                    {{ $product->category ? $product->category->name : 'Unknown' }}</span>
                                 <span class="quantity">Toko: {{ $product->toko ? $product->toko->nama_toko : 'Unknown' }}</span>
-
-                                {{-- Rating bintang --}}
-                                <div class="flex justify-center">
+                                <div class="flex">
+                                    {{-- Tampilkan bintang penuh --}}
                                     @for ($i = 1; $i <= $fullStars; $i++)
                                         <svg xmlns="http://www.w3.org/2000/svg" class="text-yellow-500 w-5 h-auto fill-current"
                                             viewBox="0 0 16 16">
@@ -108,8 +129,33 @@
                                                 d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
                                         </svg>
                                     @endfor
-                                </div>
 
+                                    {{-- Tampilkan setengah bintang jika ada --}}
+                                    @if ($halfStar)
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="text-yellow-500 w-5 h-auto fill-current"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M8 12.545L3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 12.545V0z" />
+                                        </svg>
+                                    @endif
+
+                                    {{-- Tampilkan bintang kosong --}}
+                                    @for ($i = 1; $i <= $emptyStars; $i++)
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-300 w-5 h-auto fill-current"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                        </svg>
+                                    @endfor
+
+                                    @if ($average_rating >= 1)
+                                        <p class="mx-2">( {{ $average_rating }} / 5 )</p>
+                                    @else
+                                        <p class="mx-2">( 0 / 0 )</p>
+                                    @endif
+                                </div>
+                                <span class="quantity"></span>
+                                <span class="price">Rp {{ number_format($product->price, 0, ',', '.') }}</span>
                                 <a href="javascript:void(0)" data-product-id="{{ $product->id }}"
                                     data-store-id="{{ $product->store_id }}" data-category-id="{{ $product->category_id }}"
                                     data-slug="{{ $product->slug }}" class="cart-btn">
@@ -125,11 +171,12 @@
                                 <span hidden>{{ $product->slug }}</span>
                                 <img alt="{{ $product->name }}" src="https://lapakkbk.online/{{ $product->photo }}">
                                 <strong>{{ $product->name }}</strong>
-                                <span class="quantity">Kategori: {{ $product->category ? $product->category->name : 'Unknown' }}</span>
+                                {{-- <strong>{{ $product->toko->nama_toko }}</strong> --}}
+                                <span class="quantity">Kategori:
+                                    {{ $product->category ? $product->category->name : 'Unknown' }}</span>
                                 <span class="quantity">Toko: {{ $product->toko ? $product->toko->nama_toko : 'Unknown' }}</span>
-
-                                {{-- Rating bintang --}}
-                                <div class="flex justify-center">
+                                <div class="flex">
+                                    {{-- Tampilkan bintang penuh --}}
                                     @for ($i = 1; $i <= $fullStars; $i++)
                                         <svg xmlns="http://www.w3.org/2000/svg" class="text-yellow-500 w-5 h-auto fill-current"
                                             viewBox="0 0 16 16">
@@ -137,8 +184,33 @@
                                                 d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
                                         </svg>
                                     @endfor
-                                </div>
 
+                                    {{-- Tampilkan setengah bintang jika ada --}}
+                                    @if ($halfStar)
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="text-yellow-500 w-5 h-auto fill-current"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M8 12.545L3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 12.545V0z" />
+                                        </svg>
+                                    @endif
+
+                                    {{-- Tampilkan bintang kosong --}}
+                                    @for ($i = 1; $i <= $emptyStars; $i++)
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-300 w-5 h-auto fill-current"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                        </svg>
+                                    @endfor
+
+                                    @if ($average_rating >= 1)
+                                        <p class="mx-2">( {{ $average_rating }} / 5 )</p>
+                                    @else
+                                        <p class="mx-2">( 0 / 0 )</p>
+                                    @endif
+                                </div>
+                                <span class="quantity"></span>
+                                <span class="price">Rp {{ number_format($product->price, 0, ',', '.') }}</span>
                                 <a href="javascript:void(0)" data-product-id="{{ $product->id }}"
                                     data-store-id="{{ $product->store_id }}" data-category-id="{{ $product->category_id }}"
                                     data-slug="{{ $product->slug }}" class="cart-btn">
