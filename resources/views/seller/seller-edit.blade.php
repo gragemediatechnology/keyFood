3d9f803d3a144813eb67739dbc264b358ede25a6
ArvinoDel <andikasupriyadi27@gmail.com>
2024-10-17 10:29:04 +0700

    test

--- resources/views/seller/seller-edit.blade.php
+++ resources/views/seller/seller-edit.blade.php
@@ -27,20 +27,27 @@
                                 </button>
                             </a>
                             @if ($toko->is_online)
-                                <a href="/set_status/{{ $toko->id_toko }}">
-                                    <button type="button"
-                                        class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Tutup
-                                        toko
+                                <form action="{{ route('toko.status', $toko->id_toko) }}" method="POST"
+                                    style="display: inline;">
+                                    @csrf
+                                    @method('PATCH')
+                                    <button type="submit"
+                                        class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                                        Tutup toko
                                     </button>
-                                </a>
+                                </form>
                             @else
-                                <a href="/set_status/{{ $toko->id_toko }}">
-                                    <button type="button"
-                                        class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Buka
-                                        toko
+                                <form action="{{ route('toko.status', $toko->id_toko) }}" method="POST"
+                                    style="display: inline;">
+                                    @csrf
+                                    @method('PATCH')
+                                    <button type="submit"
+                                        class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                                        Buka toko
                                     </button>
-                                </a>
+                                </form>
                             @endif
+
                         </div>
                     </div>
 
