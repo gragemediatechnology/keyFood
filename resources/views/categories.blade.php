dc5f21be3c736fb0fb87181a045eaf920db58690
Baihaqi34 <abay999mmm@gmail.com>
2024-10-21 09:47:44 +0700

    uodate

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -260,9 +260,13 @@
                                         <p>(${average_rating.toFixed(1)} / 5)</p>
                                     </div>
                                     <span class="price">Rp ${new Intl.NumberFormat('id-ID').format(product.price)}</span>
-                                    <a href="javascript:void(0)" data-product-id="${product.id}" class="cart-btn">
-                                        <i class="fas fa-shopping-bag"></i> Tambah Ke Keranjang
-                                    </a>
+                                    
+                                    ${!isTokoOnline ?
+                                    '<a href="javascript:void(0)" data-product-id="{{ $product->id }}"data-store-id="{{ $product->store_id }}" data-category-id="{{ $product->category_id }}"data-slug="{{ $product->slug }}" class="cart-btn"><i class="fas fa-shopping-bag"></i> Tambah Ke Keranjang</a>'
+                                    :
+                                    '<a href="javascript:void(0)" data-product-id="{{ $product->id }}"data-store-id="{{ $product->store_id }}" data-category-id="{{ $product->category_id }}"data-slug="{{ $product->slug }}"class="w-full h-[40px] bg-red-100 text-red-600 flex justify-center items-center mt-[20px] transition-all duration-300 ease-linear"disabled><i class="fas fa-ban"></i> Toko Tutup</a>'}
+                                    
+                                  
                                 </div>`;
                             $('#product-list').append(productHtml); // Tambahkan produk ke daftar
                         });
