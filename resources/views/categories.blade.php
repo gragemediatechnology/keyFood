3b4ddfcb55aaa795f99f7b0607fb6be3348c44c3
Baihaqi34 <abay999mmm@gmail.com>
2024-10-21 08:27:49 +0700

    update

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -150,7 +150,7 @@
         </section>
     @endsection
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-   <script>
+    <script>
     $(document).ready(function() {
         let selectedCategory = ''; // Variabel untuk menyimpan kategori yang dipilih
 
@@ -238,12 +238,16 @@
                                 starsHtml += `<svg xmlns="http://www.w3.org/2000/svg" class="text-gray-300 w-5 h-auto fill-current" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg>`;
                             }
 
+                            // Cek status toko
+                            var tokoStatus = product.toko && product.toko.isOpen ? '' : 'toko-tutup';
+                            var tokoLabel = product.toko && !product.toko.isOpen ? ' (Toko Tutup)' : '';
+
                             var productHtml = `
-                                <div class="product-box">
+                                <div class="product-box ${tokoStatus}">
                                     <img alt="${product.name}" src="${product.photo}">
                                     <strong>${product.name}</strong>
                                     <span class="quantity">Kategori: ${product.category ? product.category.name : 'Unknown'}</span>
-                                    <span class="quantity">Toko: ${product.toko ? product.toko.nama_toko : 'Unknown'}</span>
+                                    <span class="quantity">Toko: ${product.toko ? product.toko.nama_toko : 'Unknown'} ${tokoLabel}</span>
                                     <div class="flex">
                                         ${starsHtml}
                                         <p>(${average_rating.toFixed(1)} / 5)</p>
@@ -266,3 +270,4 @@
         }
     });
 </script>
+
