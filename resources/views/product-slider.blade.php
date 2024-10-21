37553456f1185b54bdfe325c75805ac7caabecba
ArvinoDel <andikasupriyadi27@gmail.com>
2024-10-21 10:32:08 +0700

    test

--- resources/views/product-slider.blade.php
+++ resources/views/product-slider.blade.php
@@ -121,10 +121,9 @@
 
             {{-- Loader untuk menampilkan saat produk sedang dimuat --}}
             <div id="loader" style="display: none;">
-                <div class="flex items-center justify-center h-screen">
-                    <div class="w-16 h-16 border-8 border-t-transparent border-blue-600 rounded-full animate-spin"></div>
+                <div class="flex items-center justify-center h-full">
+                    <div class="w-5 h-5 border-8 border-t-transparent border-blue-600 rounded-full animate-spin"></div>
                   </div>
-
             </div>
 
             <script>
@@ -201,7 +200,7 @@
                         const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
 
                         // Full stars
-                        for (let i = 0; i < fullStars; i++) {
+                        for (let i = 1; i <= fullStars; i++) {
                             starsHtml +=
                                 `<svg xmlns="http://www.w3.org/2000/svg" class="text-yellow-500 w-5 h-auto fill-current" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" /></svg>`;
                         }
@@ -213,7 +212,7 @@
                         }
 
                         // Empty stars
-                        for (let i = 0; i < emptyStars; i++) {
+                        for (let i = 1; i <= emptyStars; i++) {
                             starsHtml +=
                                 `<svg xmlns="http://www.w3.org/2000/svg" class="text-gray-300 w-5 h-auto fill-current" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" /></svg>`;
                         }
