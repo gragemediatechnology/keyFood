494b37d17ce280a109ddf1527d911dd2683e3f0d
jipyy <jahdanpandita16@gmail.com>
2024-10-22 11:04:07 +0700

    bhjhj

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,29 +1,6 @@
 <?php
 
-// use Illuminate\Foundation\Application;
-// use Illuminate\Foundation\Configuration\Exceptions;
-// use Illuminate\Foundation\Configuration\Middleware;
-
-// return Application::configure(basePath: dirname(__DIR__))
-    // ->withRouting(
-        // web: __DIR__.'/../routes/web.php',
-        // commands: __DIR__.'/../routes/console.php',
-        // health: '/up',
-    // )
-    // ->withMiddleware(function (Middleware $middleware) {
-        //
-        // $middleware->alias([
-            // 'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
-            // 'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
-            // 'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
-            // \App\Http\Middleware\TrackVisit::class,
-        // ]);
-    // })
-    // ->withExceptions(function (Exceptions $exceptions) {
-        //
-    // })->create();
-
-    use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
@@ -34,20 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Mendaftarkan middleware yang sudah ada
+        //
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
-        ]);
-
-        // Menambahkan TrackVisit sebagai named middleware
-        $middleware->alias([
-            'track.visits' => \App\Http\Middleware\TrackVisit::class,
-        ]);
-
-        // Mendaftarkan middleware global jika diperlukan
-        $middleware->group('web', [
             \App\Http\Middleware\TrackVisit::class,
         ]);
     })
