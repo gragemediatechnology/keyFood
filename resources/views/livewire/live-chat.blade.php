0c649611bfc03dc697722c8ef211c44b25acb072
ArvinoDel <andikasupriyadi27@gmail.com>
2024-10-22 10:56:23 +0700

    test

--- resources/views/livewire/live-chat.blade.php
+++ resources/views/livewire/live-chat.blade.php
@@ -97,7 +97,7 @@
                                 enctype="multipart/form-data">
                                 <textarea id="messageTextarea" class="textarea textarea-bordered text-green-500 w-full" wire:model="message"
                                     placeholder="Kirim pesan bang..." required></textarea>
-                                <input type="file" wire:model="image" class="hidden" id="imageInput" />
+                                <input type="file" wire:model="image" class="" id="imageInput" />
                                 <button type="button" id="chooseFileButton" class="btn btn-primary">Choose
                                     File</button>
                                 <button type="submit" id="submitButton" class="btn btn-primary">Kirim</button>
