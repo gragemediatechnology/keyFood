1306ef2996dcd4d4f147a29fa21947aadf68e58f
ArvinoDel <andikasupriyadi27@gmail.com>
2024-11-07 09:50:48 +0700

    anda

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -51,7 +51,7 @@
                         <ul>
                             <li>
                                 <a href="#"
-                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Menu</a>
                                 <form method="POST" action="/admin/stores/destroy/{{ $store->id_toko }}"
                                     onsubmit="return confirmDelete()">
                                     @csrf
