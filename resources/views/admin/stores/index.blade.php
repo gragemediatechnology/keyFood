d19c90eddbc371f1a1d295d84932872cf263974e
jipyy <jahdanpandita16@gmail.com>
2024-11-07 09:34:23 +0700

    m

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -18,10 +18,7 @@
                     <div class="flex items-center text-sm">
                         <!-- Avatar with inset shadow -->
                         <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-                            {{-- <img class="object-cover w-full h-full rounded-full"
-                                src="https://lapakkbk.online/store_image/{{ $store->foto_profile_toko }}"
-                                alt="{{ $store->nama_toko }}" loading="lazy" /> --}}
-                                <img class="object-cover w-full h-full rounded-full"
+                            <img class="object-cover w-full h-full rounded-full"
                                 src="https://lapakkbk.online/store_image/{{ $store->foto_profile_toko }}"
                                 alt="{{ $store->nama_toko }}" loading="lazy" />
                             <div class="absolute inset-0 rounded-full shadow-inner"
@@ -32,9 +29,6 @@
                             <button type="submit">
                                 <p class="font-semibold">{{ $store->nama_toko }}</p>
                             </button>
-                            <p class="text-xs text-gray-600 dark:text-gray-400">
-                                {{ $store->id_seller }}
-                            </p>
                         </div>
                     </div>
                     </form>
@@ -57,10 +51,23 @@
                     <div id="dropdown1" class="dropdown-menu">
                         <ul>
                             <li>
-                                <a href="#"
-                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                                <form method="POST" action="/admin/stores/destroy/{{ $store->id_toko }}"
+                                    onsubmit="return confirmDelete()">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit"
+                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-red-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                        aria-label="Delete">
+                                        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
+                                            viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd"
+                                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                                clip-rule="evenodd"></path>
+                                        </svg>
+                                    </button>
+                                </form>
                             </li>
-                            <li>
+                            {{-- <li>
                                 <a href="#"
                                     class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
                             </li>
@@ -72,7 +79,7 @@
                                 <a href="#"
                                     class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign
                                     out</a>
-                            </li>
+                            </li> --}}
                         </ul>
                     </div>
                 </div>
