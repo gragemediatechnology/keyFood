8f4f3cfc93048760c09ac1eb2e3aa0ac70e5c3eb
ArvinoDel <andikasupriyadi27@gmail.com>
2024-11-07 09:00:37 +0700

    afja

--- resources/views/term-condition.blade.php
+++ resources/views/term-condition.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://rawcdn.githack.com/jipyy/keyFood/94e3005f001914148945e309f555715db94e24f6/public/css/terms.css">
+    <link rel="stylesheet" href="https://rawcdn.githack.com/gragemediatechnology/keyFood/584a8943e4f59a35bd19192a3b6a00174c1dfb5a/public/css/terms.css">
     <script src="https://kit.fontawesome.com/3ef3559250.js" crossorigin="anonymous"></script>
     <link rel="icon" type="image/x-icon" href="../img/logos.svg">
     <title>Lapak KBK || {{ Route::currentRouteName() }} </title>
