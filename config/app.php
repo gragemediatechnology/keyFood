3c65f67686203bed752c864300216b1801e38a40
jipyy <jahdanpandita16@gmail.com>
2024-11-07 14:27:46 +0700

    a

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://lapakkbk.online'),
+    'url' => env('APP_URL', 'https://teraskabeka.com'),
 
     /*
     |--------------------------------------------------------------------------
