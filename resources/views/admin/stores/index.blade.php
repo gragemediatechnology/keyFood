37f488d79932ef4cb3d1adf5b6b4c7e622f29bb1
Baihaqi34 <abay999mmm@gmail.com>
2024-11-08 14:28:13 +0700

    sdsds

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -17,7 +17,7 @@
                             {{-- <h2>{{ $store->nama_toko }}</h2> --}}
                             <div class="flex items-center text-sm">
                                 <!-- Avatar with inset shadow -->
-                                <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
+                                <div class="relative w-8 h-8 mr-3 rounded-full md:block">
                                     <img class="object-cover w-full h-full rounded-full"
                                         src="https://teraskabeka.com/store_image/{{ $store->foto_profile_toko ? $store->foto_profile_toko : 'markets.png' }}"
                                         alt="{{ $store->nama_toko }}" loading="lazy" />
