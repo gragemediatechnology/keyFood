4e0393a2c4445f52dc3b79b44abfa45a8093423f
Baihaqi34 <abay999mmm@gmail.com>
2024-11-08 14:51:30 +0700

    asasas

--- resources/views/admin/role-requests/index.blade.php
+++ resources/views/admin/role-requests/index.blade.php
@@ -89,7 +89,7 @@
                                             <div class="flex items-center text-sm">
                                                 <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
                                                     <img class="object-cover w-full h-full rounded-full"
-                                                        src="{{ $request->img ? $request->img : 'https://teraskabeka.com/img/client-1.png' }}" alt="" loading="lazy" />
+                                                        src="https://teraskabeka.com/img/{{ $request->img ? $request->img : 'client-1.png' }}" alt="" loading="lazy" />
                                                     <div class="absolute inset-0 rounded-full shadow-inner"
                                                         aria-hidden="true">
                                                     </div>
