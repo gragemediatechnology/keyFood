cf1da437b55e2a50a659b3291810ebbb5efda50f
Baihaqi34 <abay999mmm@gmail.com>
2024-11-08 14:53:59 +0700

    sasas

--- resources/views/admin/role-requests/index.blade.php
+++ resources/views/admin/role-requests/index.blade.php
@@ -87,7 +87,7 @@
                                         <!-- ID User -->
                                         <td class="px-4 py-3">
                                             <div class="flex items-center text-sm">
-                                                <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
+                                                <div class="relative  w-8 h-8 mr-3 rounded-full md:block">
                                                     <img class="object-cover w-full h-full rounded-full"
                                                         src="https://teraskabeka.com/{{ $request->img ? $request->img : 'client-1.png' }}" alt="" loading="lazy" />
                                                     <div class="absolute inset-0 rounded-full shadow-inner"
