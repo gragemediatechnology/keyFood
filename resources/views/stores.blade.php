f4a593855473f393706cc374def26b177d15e038
jipyy <jahdanpandita16@gmail.com>
2024-11-08 13:29:37 +0700

    n

--- resources/views/stores.blade.php
+++ resources/views/stores.blade.php
@@ -8,10 +8,10 @@
     <section id="home">
 
         <div class="contain">
-            <a href="/stores"><button class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">X</button></a>
             <form class="search-bar">
                 <label for="default-search"
-                    class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                <a href="/stores"><button class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">X</button></a>
                 <div class="relative">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
