f257aca982dba23ff943e2ce0866ca12d695a432
Baihaqi34 <abay999mmm@gmail.com>
2024-11-08 15:18:50 +0700

    sasas

--- resources/views/stores.blade.php
+++ resources/views/stores.blade.php
@@ -134,7 +134,9 @@
                                         <button type="submit">
                                             <div class="container-s" id="visit">
                                                 <div class="user-s">
-                                                    <img src="store_image/${store.foto_profile_toko}" class="user-icon-s">
+                                                 <img class="user-icon-s"
+                                                src="https://teraskabeka.com/store_image/${store.foto_profile_toko ? $store->foto_profile_toko : 'markets.png'}" />
+                                                    
                                                     <div class="user-info-s">
                                                         <div class="user-name-s">${store.nama_toko}</div>
                                                         <div class="user-description-s">${store.alamat_toko}</div>
