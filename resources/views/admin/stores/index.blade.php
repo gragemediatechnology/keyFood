c62a424d4e24492ea1616c8e01be9cc58e77576c
Baihaqi34 <abay999mmm@gmail.com>
2024-11-08 14:32:06 +0700

    dadad

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -115,9 +115,9 @@
                                         <td class="px-4 py-3">
                                             <div class="flex items-center text-sm">
                                                 <!-- Avatar with inset shadow -->
-                                                <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
+                                                <div class="relative w-8 h-8 mr-3 rounded-full md:block">
                                                     <img class="object-cover w-full h-full rounded-full"
-                                                        src="https://teraskabeka.com/store_image/{{ $store->foto_profile_toko }}"
+                                                        src="https://teraskabeka.com/store_image/{{ $store->foto_profile_toko ? $store->foto_profile_toko : 'markets.png' }}"
                                                         alt="{{ $store->nama_toko }}" loading="lazy" />
                                                     <div class="absolute inset-0 rounded-full shadow-inner"
                                                         aria-hidden="true">
