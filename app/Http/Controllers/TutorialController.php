5a6ee110d33f5d22c93b5897bd8f0422948367e6
jipyy <jahdanpandita16@gmail.com>
2024-11-13 11:40:16 +0700

    dfghjkl

--- app/Http/Controllers/TutorialController.php
+++ app/Http/Controllers/TutorialController.php
@@ -33,7 +33,7 @@ class TutorialController extends Controller
     {
         // Path ke folder 'storage/app/public/vidio'
         // $videoDirectory = storage_path('app/public/vidio');
-        $videoDirectory = public_path('vidio');
+        $videoDirectory = public_path('vidio/');
 
         // Cek apakah folder ada
         if (!File::exists($videoDirectory)) {
