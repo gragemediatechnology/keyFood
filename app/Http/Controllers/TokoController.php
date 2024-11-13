b9083c8c8463386c45b0bd43edcc88afe0524dfd
Baihaqi34 <abay999mmm@gmail.com>
2024-11-13 13:50:24 +0700

    cvb

--- app/Http/Controllers/TokoController.php
+++ app/Http/Controllers/TokoController.php
@@ -43,7 +43,7 @@ class TokoController extends Controller
             $image = $request->file('foto_profile_toko');
 
             // Simpan gambar di folder 'public/img' dalam storage
-            $imagePath = $image->store('public_html/products_photo');
+            $imagePath = $image->store(base_path('public_html/products_photo'));
 
             // Simpan hanya nama file, bukan seluruh path
             $store->foto_profile_toko = basename($imagePath);
