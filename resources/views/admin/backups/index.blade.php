69fac2c509b7368e632cc085ff91c42b94dba3cb
jipyy <jahdanpandita16@gmail.com>
2024-11-19 08:36:26 +0700

    delete public html

--- resources/views/admin/backups/index.blade.php
+++ resources/views/admin/backups/index.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.layouts.main-admin')
+ @extends('admin.layouts.main-admin')
 @section('container-admin')
 <main class="h-screen overflow-y-auto">
     <div class="container px-6 mx-auto grid overflow-y-hidden py-4 mb-8">
