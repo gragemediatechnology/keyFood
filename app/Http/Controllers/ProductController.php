931ece95aec12b3bb295691b693f13dc2f0e4894
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-11-20 08:52:12 +0700

    adsf

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -115,7 +115,7 @@ class ProductController extends Controller
             ->orderBy('created_at', 'desc')
             ->paginate(5);
 
-        dd($products->nextPageUrl());
+        dd($request->root() . '/product-slider?page=' . ($products->currentPage() + 1));
 
         if ($request->ajax()) {
             return response()->json([
