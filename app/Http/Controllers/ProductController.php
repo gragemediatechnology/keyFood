e3d9c1d9606f536ada7a3e89e149851780be74a3
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-11-21 09:56:18 +0700

    dfsd

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -98,7 +98,7 @@ class ProductController extends Controller
     public function showProductSlider(Request $request)
     {
         // Mengambil data produk dengan pagination
-        $products = Product::paginate(3);
+        $products = Product::paginate(1);
 
         // Jika permintaan AJAX, kembalikan data produk dalam format JSON
         if ($request->ajax()) {
