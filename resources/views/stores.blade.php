ebc87287d5c4c31039b83ed477391c8acc54ea9a
jipyy <jahdanpandita16@gmail.com>
2024-11-22 11:13:56 +0700

    m

--- resources/views/stores.blade.php
+++ resources/views/stores.blade.php
@@ -24,7 +24,7 @@
                         placeholder="Cari Toko..." />
 
                     <button onclick="clearSearch()"
-                        class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">X</button>
+                        class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Semua Toko</button>
                 </div>
             </form>
         </div>
