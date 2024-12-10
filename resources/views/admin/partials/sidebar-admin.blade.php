43c4e9a97e47a576a9159fee1db2de1832340cab
ArvinoDel <andikasupriyadi27@gmail.com>
2024-12-10 14:33:18 +0700

    asda

--- resources/views/admin/partials/sidebar-admin.blade.php
+++ resources/views/admin/partials/sidebar-admin.blade.php
@@ -238,7 +238,7 @@
                             aria-hidden="true"></span>
                     @endif
                     <a class="{{ Request::is('admin/history') ? 'inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100' : 'inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200' }}"
-                        href="/admin/history"> 
+                        href="/admin/history">
                         <i class='bx bx-history bx-sm'></i>
                         <span class="ml-4">Histories</span>
                     </a>
@@ -270,13 +270,13 @@
                     </li>
                 </ul>
             </ul>
-            <div class="px-6 my-6">
+            {{-- <div class="px-6 my-6">
                 <button
                     class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
                     Create account
                     <span class="ml-2" aria-hidden="true">+</span>
                 </button>
-            </div>
+            </div> --}}
         </div>
     </aside>
     <div class="flex flex-col flex-1 w-full">
