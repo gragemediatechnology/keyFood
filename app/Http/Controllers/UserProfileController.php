b1cac1f6a1ae13e06d57500ecf4568c3cc7f9da2
jipyy <jahdanpandita16@gmail.com>
2024-12-11 13:27:07 +0700

    eee

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -104,25 +104,33 @@ public function update(Request $request)
     }
 }
     
-    public function destroy()
-{
-    $user = Auth::user();
 
-    // Check if the user has the 'seller' role
+public function destroy()
+{ 
+$user = Auth::user();
+// dd($user);
+
+   
     if ($user->hasRole('seller')) {
-        // Reset roles to no roles or a default role
-        $user->syncRoles([]); // Atur ke kosong atau bisa assign role default jika ada
-
-        // Optionally, delete the user's related products and store (handled in the model)
+        // Hapus produk terkait berdasarkan creator_id
         $user->products()->delete();
+    
+        // Hapus toko terkait
         $user->store()->delete();
+    
+        // Reset role
+        $user->syncRoles([]);
     }
+    
+    
 
-    // Logout and delete the user
+    // // Logout and delete the user
     Auth::logout();
     $user->delete();
 
-    return redirect('/')->with('success', 'Account deleted successfully.');
+    return redirect('/home')->with('Berhasil', 'Akun anda berhasil di hapus.');
 }
+
+
 
 }
