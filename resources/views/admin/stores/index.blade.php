ac923dc72e5df09adf2c432cde4622895409d0f3
Baihaqi34 <abay999mmm@gmail.com>
2024-12-18 16:06:11 +0700

    sasas

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -162,7 +162,7 @@
     $(document).ready(function() {
     var loading = false;  // Flag to prevent multiple AJAX calls
     var nextPage = {{ $stores->currentPage() }} + 1;  // Start with the next page after the initial load
-    var itemsPerPage = 5;  // Adjust this based on your pagination settings
+    var itemsPerPage = 2;  // Adjust this based on your pagination settings
 
     console.log(nextPage);
     console.log(itemsPerPage);
